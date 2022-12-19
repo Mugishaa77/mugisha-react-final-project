7ea04cbc463dcfb603349ca21a2e99d0ab199f92
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2022-12-19 13:50:33 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h2>Weathwr App</h2>
+      <h2>Weather App</h2>
       <footer>
-        This project was coded by Mugisha and is
+        This project was coded by Mugisha and is {" "}
       <a href="https://github.com/Mugishaa77/mugisha-react-final-project"
-      target="_blank">open-sourced on github</a>
+      target="_blank"
+      rel="noreferrer"> {" "}open-sourced on github</a>
       </footer>
       
     </div>
