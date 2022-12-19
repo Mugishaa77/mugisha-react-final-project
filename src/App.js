73073091d77fc32037704d542d20c925e8e61f4b
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2022-12-19 13:28:52 +0300

    Further deleted unneccessary files and fixed a bug

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
+      <h2>Weathwr App</h2>
+      <footer>
+        This project was coded by Mugisha and is
+      <a href="https://github.com/Mugishaa77/mugisha-react-final-project"
+      target="_blank">open-sourced on github</a>
+      </footer>
       
     </div>
   );
 }
 
-export default App;
+
