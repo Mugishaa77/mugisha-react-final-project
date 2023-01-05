cfdc28b24ea2cc3cd0e5cac4a77dd672e72227d1
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-01-05 13:15:08 +0300

    updated the html content of the file

--- src/Weather.js
+++ src/Weather.js
@@ -1,4 +1,5 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.css';
 import "./Weather.css";
 
 
@@ -6,30 +7,54 @@ import "./Weather.css";
   return (
   <div className = "Weather">
     <form>
-        <div className="col-9">
+        <div className="row">
+            <div className="col-9">
             <input type="text" placeholder="Enter city here" className="form-ctrl"/>
         </div>
         <div className = "col-3">
             <input type="submit" value="Search" className="btn"/>
         </div>
-        
-    </form>
-    <h1>City</h1>
-    <ul>
-        <li>Day and date</li>
-        <li>Description</li>
-    </ul>
-    <div className = "row">
-        <div className = "col-6">
-            <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt = "weather symbol" />
         </div>
-        <div className = "col-6">
+    </form>
+
+    <div className="row">
+        <div className = "col-4">
             <ul>
-                <li>Preciptitation</li>
-                <li>humidity</li>
-                <li>wind</li>
+                <li>
+                    <img src="" alt=""/>
+                </li>
+                <li>
+                    Temperature
+                </li>
+                <li>
+                    Description
+                </li>
             </ul>
+        </div>
+        <div className = "col-3">
+            Wind
+        </div>
+        <div className = "col-2">
+            Humidity
+        </div>
+        <div className = "col-3">
+            Precipitation
+        </div>
+    </div>
+    
+    <div className = "row">
+        <div className = "col-4">
+            <ul>
+                <li>
+                    <h1>
+                        City
+                    </h1>
+                </li>
+                <li> Day and date</li>
+            </ul>
+        </div>
+        <div className = "col-8">
+            Weather forecast
         </div>
     </div>
     </div>
